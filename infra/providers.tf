288bb3cdc71afb97cda0e7018e18a36ace985d90
dvir gross <dvirgross2000@gmail.com>
2023-05-26 22:17:15 +0300

    added s3 backend to a gcp infra :)]

--- infra/providers.tf
+++ infra/providers.tf
@@ -8,6 +8,13 @@ terraform {
       version = "4.66.0"
     }
   }
+  backend "s3" {
+    bucket         = "dvir-tf-state"
+    key            = "dvir-cluster/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "tf-state"
+    encrypt        = true
+  }
 #   backend "gcs" {
 #     bucket = "<your-bucket>"
 #     prefix = "terraform/state"
